d2c131ec0ee5f4a92b45249fea570753370d28f0
covmoore <covmoore5@gmail.com>
2024-07-21 14:18:47 -0700

    made basic projectile script and added particles

--- handlers/Player.gd
+++ handlers/Player.gd
@@ -4,11 +4,17 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const SENSITIVITY = 0.003
+var mouse_mode = "captured"
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var bullet = load("res://model_scenes/handgun_projectile.tscn")
+var bullet_instance
+
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
+@onready var gun_anim = $Head/Camera3D/TemHandGun/AnimationPlayer
+@onready var gun_barrel = $Head/Camera3D/TemHandGun/RayCast3D
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -18,6 +24,14 @@ func _unhandled_input(event):
 		head.rotate_y(-event.relative.x * SENSITIVITY)
 		camera.rotate_x(-event.relative.y * SENSITIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-45), deg_to_rad(60))
+		
+	if Input.is_action_just_pressed("pause"):
+		if mouse_mode == "captured":
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			mouse_mode = "hidden"
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			mouse_mode = "captured"
 		
 func _physics_process(delta):
 	# Add the gravity.
@@ -40,7 +54,11 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	
 	if Input.is_action_just_pressed("shoot"):
-		#do something
-		print("SHOOT")
+		if !gun_anim.is_playing():
+			gun_anim.play("Shoot")
+			bullet_instance = bullet.instantiate()
+			bullet_instance.position = gun_barrel.global_position
+			bullet_instance.basis = gun_barrel.global_transform.basis
+			get_parent().add_child(bullet_instance)
 
 	move_and_slide()
