803d300bb2d4995e1b43cff791fee6e4f36acf51
Nicholas Facciola <nicholasfacciola@gmail.com>
2024-07-23 21:02:50 -0700

    fixed ui anchor bug

--- UI/PlayerUI.gd
+++ UI/PlayerUI.gd
@@ -1,11 +1,13 @@
 extends Control
 
-@onready var health_text = $healthTxt
-@onready var game_over_UI = $gameOverTxt
+@onready var health_text = $CanvasLayer/healthTxt
+@onready var game_over_text = $CanvasLayer/gameOverTxt
 
+func _ready():
+	game_over_text.visible = false
 
 func _on_player_player_died():
-	game_over_UI.visible = true
+	game_over_text.visible = true
 
 func _on_player_player_hit(dmg):
 	health_text.text = "%1d" % dmg
