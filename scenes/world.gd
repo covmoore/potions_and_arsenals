fa3adca4631d3d5fe1cb609144c1c37c06c65e36
Nicholas Facciola <nicholasfacciola@gmail.com>
2024-07-23 20:40:02 -0700

    moved UI actions to PlayerUI script

--- scenes/world.gd
+++ scenes/world.gd
@@ -1,7 +1,5 @@
 extends Node3D
 
-@onready var health_UI = $Control/healthTxt
-@onready var game_over_UI = $Control/gameOverTxt
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,11 +9,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _on_player_player_hit(dmg):
-	health_UI.text = "%1d" % dmg
-
-
-func _on_player_player_died():
-	game_over_UI.visible = true
