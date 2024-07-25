de26d641540093840f3f47dba06ab17c1b68f648
Nicholas Facciola <nicholasfacciola@gmail.com>
2024-07-24 18:48:45 -0700

    peaceful mode

--- UI/slot.gd
+++ UI/slot.gd
@@ -4,4 +4,4 @@ extends ColorRect
 func _on_gui_input(event):
 	if event is InputEventMouseButton:
 		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-			print("slot was clicked")
+			print(self.name + " was clicked")
--- scenes/world.gd
+++ scenes/world.gd
@@ -1,9 +1,15 @@
 extends Node3D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+enum DIFFICULTY {
+	PEACEFUL,
+	NORMAL
+}
 
+var game_difficulty = DIFFICULTY.NORMAL
+
+
+func _ready():
+	game_difficulty = DIFFICULTY.PEACEFUL
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
