f7346e42ce0129a7fc510d81bae915f6a7599378
Nicholas Facciola <nicholasfacciola@gmail.com>
2024-07-25 21:34:31 -0700

    merging with main

--- Alchemy/AlchemyItem.gd
+++ Alchemy/AlchemyItem.gd
@@ -1,13 +1,18 @@
 extends Area3D
 
 #simulate gravity
-@export var gravity_strength = -1080
+@export var gravity_strength = -250
+
+signal item_picked_up
 var velocity = Vector3.ZERO
+var player_inventory = null
 var is_on_ground = false
 
 func _ready():
 	velocity = Vector3.ZERO
-	
+	player_inventory = find_child("Inventory")
+	print(player_inventory)
+	item_picked_up.connect(_on_item_picked_up)
 
 func _physics_process(delta):
 	if not is_on_ground:
@@ -19,8 +24,11 @@ func _physics_process(delta):
 	velocity.y = 0
 
 func _on_body_entered(body):
-	if body is CharacterBody3D or body is RigidBody3D:
-		print("Collision detected with: ", body.name)
+	if body.name == "Player":
+		emit_signal("item_picked_up")
 	else:
 		if body.name == "Ground":
 			is_on_ground = true
+
+func _on_item_picked_up():
+	print("Item picked up!")
--- UI/Inventory.gd
+++ UI/Inventory.gd
@@ -1,5 +1,7 @@
 extends GridContainer
 
+func add_item():
+	print('added item')
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
