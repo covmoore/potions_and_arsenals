c298d670bc7add5a13f65bf07f5f1c1271402c4b
Nicholas Facciola <nicholasfacciola@gmail.com>
2024-07-29 00:21:05 -0700

    working on more than 1 item

--- UI/SlotNotNull.gd
+++ UI/SlotNotNull.gd
@@ -27,16 +27,25 @@ func _on_gui_input(event):
 				else:
 					print(parent_name)
 
-func get_inventory_slot_index(inventory_parnet_node, slot_node_name) -> int:
+func get_inventory_slot_index(parnet_node, slot_node_name) -> int:
 	var idx = -1
-	for i in range(inventory_parnet_node.get_child_count()):
-		if inventory_parnet_node.get_child(i).name == slot_node_name:
+	for i in range(parnet_node.get_child_count()):
+		if parnet_node.get_child(i).name == slot_node_name:
 			idx = i
 			return idx
 	return idx
 
+func get_ingredient_slot_index(parent_node, slot_node_name) -> int:
+	var idx = -1
+	for i in range(parent_node.get_child_count()):
+		if parent_node.get_child(i).name.split(" ").size() == 2:
+			if parent_node.get_child(i).name.split(" ")[1] == slot_node_name:
+				idx = i
+				return idx
+	return idx
+
 func handle_inventory_click():
-	world_instance.debug_print("click came from inventory", true)
+	world_instance.debug_print("click came from inventory")
 	#check if there is empty slot in the ingredients
 	var ingredients_slots = self.get_tree().get_root().get_node("World/PlayerUI/CanvasLayer/AlchemyPanel/IngredientsMarginContainer/IngredientsHBoxContainer/Ingredients")
 	
@@ -51,32 +60,53 @@ func handle_inventory_click():
 			var parent_invenotry_node = self.get_parent()
 			var inventory_slot_idx = get_inventory_slot_index(parent_invenotry_node, self.name)
 			if inventory_slot_idx != -1:
-				#remove both nodes from their parents
-				parent_invenotry_node.remove_child(self)
-				ingredients_slots.remove_child(current_slot)
-				#add the cureent slot node to the inventory
-				parent_invenotry_node.add_child(current_slot)
-				parent_invenotry_node.move_child(current_slot, inventory_slot_idx)
+				#check count of inventory_item
+				var item_count = int((self.get_child(0).text))
+				var has_more_than_1_of_item = item_count > 1
+				if not has_more_than_1_of_item:
+					#remove both nodes from their parents
+					parent_invenotry_node.remove_child(self)
+					ingredients_slots.remove_child(current_slot)
+					#add the cureent slot node to the inventory
+					parent_invenotry_node.add_child(current_slot)
+					parent_invenotry_node.move_child(current_slot, inventory_slot_idx)
+				else:
+					#remove 1 from item count
+					self.get_child(0).text = str(int((self.get_child(0).text)) - 1)
+					current_slot.queue_free()
 				#create a control node to hold this node
 				var control_node = Control.new()
 				control_node.set_custom_minimum_size(Vector2(80,80))
-				control_node.name = "Ingredient" + self.name
+				control_node.name = "Ingredient " + self.name
 				ingredients_slots.add_child(control_node)
-				control_node.add_child(self)
+				control_node.add_child(self.duplicate())
 				ingredients_slots.move_child(control_node, ingredient_slot_index)
+				if has_more_than_1_of_item:
+					control_node.get_child(0).get_child(0).text = "1"
 				break
-		
-	#for slot in ingredients_slots.get_children():
-		##check if any of the slots are empty
-		#if slot.name.begins_with("Ingredient"):
-			##swap the clicked slot with the ingredients slot
-			#var temp_slot = slot.duplicate()
-			#var parent_inventory = self.get_parent()
-			#parent_inventory.remove_child(self)
-			#ingredients_slots.add_child(self)
-			#ingredients_slots.remove_child(slot)
-			#parent_inventory.add_child(temp_slot)
-			#break
 
 func handle_ingredients_click():
-	world_instance.debug_print("click came from ingredients")
+	world_instance.debug_print("click came from ingredients", true)
+	#move the item back to the inventory
+	var inventory_slots = self.get_tree().get_root().get_node("World/PlayerUI/CanvasLayer/AlchemyPanel/InventoryMarginContainer/InventoryHBoxContainer/InventoryVBoxContainer/Inventory")
+	
+	for i in range(inventory_slots.get_child_count()):
+		var current_slot = inventory_slots.get_child(i)
+		if current_slot.name.begins_with("Ingredient"):
+			var inventory_slot_index = i
+			var parent_ingredient_node = self.get_parent().get_parent()
+			var ingredient_slot_idx = get_ingredient_slot_index(parent_ingredient_node, self.name)
+			if ingredient_slot_idx != -1:
+				#remove both nodes from their parents
+				var parent = self.get_parent()
+				parent_ingredient_node.remove_child(parent)
+				inventory_slots.remove_child(current_slot)
+				#add the current slot node to the inventory
+				parent_ingredient_node.add_child(current_slot)
+				parent_ingredient_node.move_child(current_slot, ingredient_slot_idx)
+
+				parent.remove_child(self)
+				inventory_slots.add_child(self)
+				inventory_slots.move_child(self, inventory_slot_index)
+				parent.queue_free()
+				break
